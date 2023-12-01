84e237b276107f7261cbf4cf3649f2af8c1e0423
Andrew Downey <andrew.downey@wearepatchworks.com>
2023-12-01 12:38:15 +0000

    Day 1

--- day_01/part1/1.py
+++ day_01/part1/1.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 import os
+import re
 from time import time
 
 
 def main():
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, '../example.txt')
+    filename = os.path.join(dirname, "../input.txt")
     with open(filename, "r") as myfile:
         rows = myfile.read().splitlines()
 
-    print(f"Rows: {rows}")
+    total = 0
+    for row in rows:
+        digits = re.findall(r"\d", row)
+        firstDigit = digits.pop(0)
+        newNumber = firstDigit + (digits.pop() if len(digits) > 0 else firstDigit)
+        print(f"Adding new number: {newNumber}")
+        total += int(newNumber)
+
+    return total
 
 
 if __name__ == "__main__":
-    challenge_start_time= time()
-    main()
+    challenge_start_time = time()
+    result = main()
     challenge_end_time = time()
-    print(f'Finished in {round((challenge_end_time  - challenge_start_time) * 1000, 5)} milliseconds')
+    print(f"Result: {result}")
+    print(
+        f"Finished in {round((challenge_end_time  - challenge_start_time) * 1000, 5)} milliseconds"
+    )
--- day_01/part2/2.py
+++ day_01/part2/2.py
@@ -1,19 +1,50 @@
 #!/usr/bin/env python3
 import os
+import re
 from time import time
+
+wordDict = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
 
 
 def main():
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, '../example.txt')
+    filename = os.path.join(dirname, "../input.txt")
     with open(filename, "r") as myfile:
         rows = myfile.read().splitlines()
 
-    print(f"Rows: {rows}")
+    regex = "(?=(\d|" + "|".join(wordDict) + "))"
+
+    total = 0
+    for row in rows:
+        digits = re.findall(regex, row)
+        firstDigit = digits.pop(0)
+        secondDigit = digits.pop() if len(digits) > 0 else firstDigit
+
+        firstDigit = firstDigit if firstDigit.isdigit() else wordDict[firstDigit]
+        secondDigit = secondDigit if secondDigit.isdigit() else wordDict[secondDigit]
+
+        newNumber = firstDigit + secondDigit
+        print(f"{newNumber} for {row}")
+        total += int(newNumber)
+
+    return total
 
 
 if __name__ == "__main__":
-    challenge_start_time= time()
-    main()
+    challenge_start_time = time()
+    result = main()
     challenge_end_time = time()
-    print(f'Finished in {round((challenge_end_time  - challenge_start_time) * 1000, 5)} milliseconds')
+    print(f"Result: {result}")
+    print(
+        f"Finished in {round((challenge_end_time  - challenge_start_time) * 1000, 5)} milliseconds"
+    )
